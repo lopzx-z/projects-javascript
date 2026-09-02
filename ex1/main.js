7a044f7b2e52093e8deef225982aac8f4edfce1b
Thiago <Thiago.h.Lopes@outlook.com>
2026-09-02 18:08:44 -0300

    Implementação do css e refatoração do código

--- ex1/main.js
+++ ex1/main.js
@@ -2,13 +2,23 @@ const form = document.getElementById("form");
 const mostrarTexto = document.getElementById("msg");
 
 // Função de validação de idade
+
 function verifIdade(idade) {
-    if (idade <= 0 || idade >= 120) {
+    if (idade < 0 || idade >= 120) {
         mostrarTexto.innerText = "Idade incorreta.";
+        mostrarTexto.style.color = "red";
+
     } else if (idade >= 18) {
-        mostrarTexto.innerText = "Maior de idade, acesso ok.";
+        mostrarTexto.innerText = "Maior de idade, acesso liberado.";
+        mostrarTexto.style.color = "green";
+
+    } else if (idade == 0) {
+        mostrarTexto.innerText = "Sua idade não pode ser 0";
+        mostrarTexto.style.color = "red";
+
     } else {
         mostrarTexto.innerText = "Menor de idade, acesso negado.";
+        mostrarTexto.style.color = "red";
     }
 }
 
